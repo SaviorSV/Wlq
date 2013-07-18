453de53c761d9255a7906535f1036defb363b80d
leo <010227leo@gmail.com>
2013-07-18 10:57:56 +0800

    init dev solution

---
+++ Wlq.Core.Implementation/CommonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hanger.Common;
+using Wlq.Core.Services;
+
+namespace Wlq.Core.Implementation
+{
+	public class CommonService : Disposable, ICommonService
+	{
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		protected override void InternalDispose()
+		{
+			
+		}
+	}
+}
---
+++ Wlq.Core.Implementation/RegisterServiceTask.cs
@@ -0,0 +1,21 @@
+﻿using Hanger.Common;
+using Microsoft.Practices.Unity;
+using Wlq.Core.Services;
+
+namespace Wlq.Core.Implementation
+{
+	public class RegisterServiceTask : RegisterServiceBootstrapperTask
+	{
+		public RegisterServiceTask(IUnityContainer container) : base(container) { }
+
+		public override void Execute()
+		{
+			container.RegisterTypeAsSingleton<ICommonService, CommonService>();
+		}
+
+		protected override void InternalDispose()
+		{
+			container.Resolve<ICommonService>().Dispose();
+		}
+	}
+}
