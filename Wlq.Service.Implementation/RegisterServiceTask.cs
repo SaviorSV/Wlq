e644635da33c631c64693e85172a538c04104334
leo <010227leo@gmail.com>
2013-08-12 13:43:27 +0800

    add services

--- Wlq.Service.Implementation/RegisterServiceTask.cs
+++ Wlq.Service.Implementation/RegisterServiceTask.cs
@@ -11,12 +11,14 @@ namespace Wlq.Service.Implementation
 		public override void Execute()
 		{
 			container.RegisterTypeAsPerResolve<ICommonService, CommonService>();
+			container.RegisterTypeAsPerResolve<IPostService, PostService>();
 			container.RegisterTypeAsPerResolve<IUserService, UserService>();
 		}
 
 		protected override void InternalDispose()
 		{
 			container.Resolve<ICommonService>().Dispose();
+			container.Resolve<IPostService>().Dispose();
 			container.Resolve<IUserService>().Dispose();
 		}
 	}
