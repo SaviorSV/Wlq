91915f2309c7d099d9aae855d553e551d85fe0e5
leo <010227leo@gmail.com>
2013-09-06 15:46:28 +0800

    update model

--- Wlq.Web/Models/GroupDetailModel.cs
+++ Wlq.Web/Models/GroupDetailModel.cs
@@ -4,10 +4,8 @@ using Wlq.Domain;
 
 namespace Wlq.Web.Models
 {
-	public class GroupDetailModel
+	public class GroupDetailModel : GroupModel
 	{
-		public GroupInfo Group { get; set; }
-		public bool IsFollowing { get; set; }
 		public IEnumerable<PostModel> Posts { get; set; }
 	}
 }
