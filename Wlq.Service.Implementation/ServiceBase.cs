b4906774a8aab7d3806e7b9c2e0c36b4077108ab
leo <010227leo@gmail.com>
2013-09-06 12:40:31 +0800

    update

--- Wlq.Service.Implementation/ServiceBase.cs
+++ Wlq.Service.Implementation/ServiceBase.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 using Hanger.Common;
-using Wlq.Persistence;
 using Wlq.Domain;
+using Wlq.Persistence;
 
 namespace Wlq.Service.Implementation
 {
