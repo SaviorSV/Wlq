63d07deb8c8fb2e36515b95a022032981244cb72
leo <010227leo@gmail.com>
2013-09-06 13:54:15 +0800

    add CacheHelper

--- Wlq.Service/Utility/FileManager.cs
+++ Wlq.Service/Utility/FileManager.cs
@@ -118,4 +118,27 @@ namespace Wlq.Service.Utility
 			}
 		}
 	}
+
+	public enum ThumbnailMode
+	{
+		/// <summary>
+		/// 指定高宽缩放（可能变形）
+		/// </summary>
+		HeightWidth = 1,
+
+		/// <summary>
+		/// 指定宽，高按比例
+		/// </summary>
+		Width = 2,
+
+		/// <summary>
+		/// 指定高，宽按比例
+		/// </summary>
+		Height = 3,
+
+		/// <summary>
+		/// 指定高宽裁减（不变形）
+		/// </summary>
+		Cut = 4
+	}
 }
