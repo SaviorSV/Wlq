402658bc612cb7aab7ac125f209b547b7fd3b5c2
neo1989 <neosu89@gmail.com>
2013-09-07 23:28:59 +0800

    venue booking

---
+++ Wlq.Web/Content/js/extend.js
@@ -0,0 +1,128 @@
+﻿(function ($) {
+
+    var data = null;
+    var period_begin = 2400;
+    var period_end = 0;
+    var thead_data = new Array();
+
+    function initialize(option) {
+        data = option
+        for (var i in data) {
+            thead_data[i] = data[i]['Date'];
+            for (var j in data[i]['Periods']) {
+                begin_time = parseInt(data[i]['Periods'][j]['BeginTime'])
+                end_time = parseInt(data[i]['Periods'][j]['EndTime'])
+                if (period_begin > begin_time) {
+                    period_begin = begin_time
+                }
+                if (period_end < end_time) {
+                    period_end = end_time
+                }
+            }
+        }
+    }
+
+    function createHead() {
+        var thead = $('<thead></thead>');
+        var tr = $('<tr></tr>');
+        tr.append($('<th></th>').addClass('none'))
+        for (var i in thead_data) {
+            str = thead_data[i].substring(5, 10)
+            tr.append($('<th>' + str + '</th>'))
+        }
+        thead.append(tr)
+        return thead
+    }
+
+    function createBody() {
+        var tbody = $('<tbody></tbody>');
+
+        for (var i = period_begin; i < period_end; i += 100) {
+            var tr = $('<tr></tr>');
+            t = i / 100;
+            t_period = t.toString() + ':00-' + (t + 1).toString() + ':00'
+            tr.append($('<th>' + t_period + '</th>'))
+            for (var j in data) {
+                var rowspan = 0;
+                var attrs = null;
+                for (var k in data[j]['Periods']) {
+                    var begin = parseInt(data[j]['Periods'][k]['BeginTime']);
+                    var end = parseInt(data[j]['Periods'][k]['EndTime']);
+                    rowspan = getRow(i, begin, end);
+                    attrs = getAttrs(i, data[j]['Periods'][k])
+                }
+                var td = null;
+                if (rowspan > 0) {
+                    td = $('<td></td>').attr('rowspan', rowspan);
+                }
+                else if (rowspan == 0) {
+                    td = $('<td></td>').addClass('off');
+                }
+
+                if (attrs != null) {
+                    attrs['Date'] = data[j]['Date']
+                    for (var key in attrs) {
+                        td.attr(key, attrs[key]);
+                    }
+
+                    if (attrs['LimitNumber'] > attrs['BookingNumber'] && !attrs['IsBooked']) {
+                        td.addClass('on');
+                        td.text('可预订');
+                    }
+                    else if (attrs['IsBooked']) {
+                        td.addClass('already');
+                        td.text('已预订');
+                    }
+                    else {
+                        td.addClass('off');
+                        td.text('不可预订');
+                    }
+                }
+
+                tr.append(td);
+            }
+
+            tbody.append(tr)
+        }
+        return tbody
+    }
+
+    function getRow(t, begin, end) {
+        if (t > begin && t < end)
+            return -1;
+        else if (t != begin) {
+            return 0;
+        }
+        else {
+            return (end - begin) / 100
+        }
+    }
+
+    function getAttrs(t, data) {
+        if (t == parseInt(data['BeginTime'])) {
+            var attrs = new Array();
+            attrs['BookingNumber'] = data['BookingNumber'];
+            attrs['IsBooked'] = data['IsBooked'];
+            attrs['LimitNumber'] = data['LimitNumber'];
+            attrs['VenueConfigId'] = data['VenueConfigId'];
+            return attrs;
+        }
+        else
+            return null;
+    }
+
+    $.fn.extend({
+        booking: function (option) {
+            initialize(option)
+
+            var table = $('<table></table>').addClass('cd-detail-tbl');
+
+            table.append(createHead())
+            table.append(createBody())
+
+            this.empty().append(table)
+        }
+    });
+
+
+})(jQuery);
